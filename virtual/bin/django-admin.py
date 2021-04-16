24af446738822440ca546e1f251f07e96187fab5
Michel Atieno <mishqamish@gmail.com>
2021-04-16 11:23:23 +0300

    Initial Setup

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/michel/Desktop/Machine Learning/Twitter_React/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
