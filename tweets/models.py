beff24b12c7e99cd8701b7c69fe861569cf1486c
Michel Atieno <mishqamish@gmail.com>
2021-04-21 16:24:46 +0300

    Added user to Tweet table, added admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,12 +1,17 @@
 import random
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
-
+    
+    
     class Meta:
         ordering = ['-id']
     
