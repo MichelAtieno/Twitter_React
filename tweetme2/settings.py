c4da081f7ecbb65518bf206e9a82e13a37829511
Michel Atieno <mishqamish@gmail.com>
2021-04-28 11:49:21 +0300

    Fixed error in database

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'tweetme2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'twitter',
+        'NAME': 'twitter_app',
         'USER': 'michel',
     'PASSWORD': 'mishy',
     }
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
