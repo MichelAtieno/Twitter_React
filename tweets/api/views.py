96ce3cfcd084759742e3005cc144d15fb7325271
Michel Atieno <mishqamish@gmail.com>
2021-05-21 10:53:33 +0300

    Tweets Paginated

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
 from ..forms import TweetForm
@@ -78,29 +79,30 @@ def tweet_action_view(request, *args, **kwargs):
             return Response(serializer.data, status=201)
           
     return Response({}, status=200)
-   
 
+def get_paginated_queryset_response(qs, request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 20
+    paginated_qs = paginator.paginate_queryset(qs, request)
+    serializer = TweetSerializer(paginated_qs, many=True)
+   
+    return paginator.get_paginated_response(serializer.data)
+
+   
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def tweet_feed_view(request, *args, **kwargs):
-    user = request.user
-    profiles = user.following.all()
-    followed_users_id = []
-    if profiles.exists():
-        followed_users_id = [x.user.id for x in profiles]
-    followed_users_id.append(user.id)
-    qs = Tweet.objects.filter(user__id__in=followed_users_id).order_by("-timestamp")
-    serializer = TweetSerializer(qs, many=True)
    
-    return Response(serializer.data, status=200)
+    user = request.user
+    qs = Tweet.objects.feed(user)
+   
+    return get_paginated_queryset_response(qs, request)
 
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     username = request.GET.get('username')
     if username != None:
-        qs = qs.filter(user__username__iexact=username)
-    serializer = TweetSerializer(qs, many=True)
-   
-    return Response(serializer.data, status=200)
+        qs = qs.by_username(username)
+    return get_paginated_queryset_response(qs, request)
 
