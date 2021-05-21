eb5bb2104f93304ca86be012cd3b7f5349d7d2ed
Michel Atieno <mishqamish@gmail.com>
2021-05-21 10:04:01 +0300

    Tweet feed view working

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -29,6 +29,10 @@ def user_follow_view(request, username, *args, **kwargs):
     me = request.user
     other_user_qs = User.objects.filter(username=username)
     
+    if me.username == username:
+        my_followers = me.profile.followers.all()
+        return Response({"count": my_followers.count()}, status=200)
+
     if not other_user_qs.exists():
         return Response({}, status=400)
     other = other_user_qs.first()
