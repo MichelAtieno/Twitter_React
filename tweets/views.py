9eba8bdf5e94a9dd5eeb81a7bb5b24e148599757
Michel Atieno <mishqamish@gmail.com>
2021-05-25 13:16:49 +0300

    Updated static files, Ran final build

--- tweets/views.py
+++ tweets/views.py
@@ -16,60 +16,6 @@ def tweets_list_view(request, *args, **kwargs):
    
     return render(request, "tweets/list.html")
 
-def tweets_detail_view(request, tweet_id, *args, **kwargs):
+def tweets_detail_view(request, id, *args, **kwargs):
     
-    return render(request, "tweets/detail.html", context={"tweet_id": tweet_id})
-
-
-def tweet_create_view_pure_django(request, *args, **kwargs):
-    user = request.user
-    if not request.user.is_authenticated:
-        user = None
-        if request.is_ajax():
-            return JsonResponse({}, status=401)
-        return redirect(settings.LOGIN_URL)
-    
-    form = TweetForm(request.POST or None)
-    next_url = request.POST.get("next") or None
-    if form.is_valid():
-        obj = form.save(commit=False)
-        obj.user = user
-        obj.save()
-        if request.is_ajax():
-            return JsonResponse(obj.serialize(), status=201)
-        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
-            return redirect(next_url)
-        form = TweetForm()
-    if form.errors:
-        if request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-    
-    return render(request, 'components/forms.html', context={"form": form})
-
-def tweet_list_view_pure_django(request, *args, **kwargs):
-    qs = Tweet.objects.all()
-    tweets_list = [x.serialize() for x in qs]
-    data = {
-        "isUser": False,
-        "response": tweets_list
-    }
-    return JsonResponse(data)
-
-def tweet_detail_view_pure_django(request, tweet_id, *args, **kwargs):
-    """
-    REST API VIEW
-    Consume by JS, Swift, Java, iOS, Android
-    return JSON data
-    """
-    data = {
-        "id": tweet_id,
-    }  
-    status = 200
-    try:
-        obj = Tweet.objects.get(id=tweet_id)
-        data['content'] = obj.content
-    except:
-        data['message'] = "Not found"
-        status = 404
-
-    return JsonResponse(data, status=status)
+    return render(request, "tweets/detail.html", context={"id":id})
